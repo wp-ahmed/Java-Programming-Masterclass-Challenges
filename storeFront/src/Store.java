2aedaf5dd92f80c471263dc51cce00d6363f0a96
Ahmed Samir <54330098+wp-ahmed@users.noreply.github.com>
2023-02-17 12:12:01 +0200

    Update Store.java

--- storeFront/src/Store.java
+++ storeFront/src/Store.java
@@ -10,12 +10,14 @@ public class Store {
     public Store(String name) {
         this.name = name;
     }
+    
     public void addProduct(String name, String type, double price, String description) {
 
         if(getProduct(name) != null) {
             System.out.println("The product is already existed in the store");
             return;
         }
+        
         name = name.toLowerCase();
         switch (name) {
             case "chips" -> products.add(new Chips(name,price,description));
@@ -30,39 +32,51 @@ public class Store {
                 }
             });
         }
+        
         System.out.println("The product has been added successfully to the store");
     }
 
     public void addOrder(String productName) {
+        
         if(getProduct(productName) == null) {
             System.out.println("Sorry the product is not available right now");
             return;
         }
+        
         ProductForSale product = getProduct(productName);
         orderItem order = new orderItem(product);
         orders.add(order);
     }
+    
     private ProductForSale getProduct(String productName) {
+        
         productName = productName.toLowerCase();
+        
         for(var product : products) {
             if(product.getName().equalsIgnoreCase(productName)){
                 return product;
             }
         }
+        
         return null;
     }
     public void printAllProducts() {
+        
         System.out.println("All products detail");
         System.out.println("-".repeat(30));
+        
         for (var product : products){
             product.showDetails();
             System.out.println("-".repeat(30));
         }
+        
     }
     public void printAllOrders(){
+        
         int orderNumber = 1;
         System.out.println("All order details");
         System.out.println("-".repeat(30));
+        
         for (var order : orders) {
             System.out.println("order number: "+orderNumber);
             order.product().showDetails();
